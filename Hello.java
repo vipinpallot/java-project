1e28782282cf47c24983434581691beafdde2561
edureka <support@edureka.co>
2024-07-23 16:58:33 +0000

    Second commit of Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 //
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Welcome to Git's Online Training!");
+    System.out.println("Welcome to DevOps Certification Training Programme!");
   }
 }
